245b84798b7f274c61aa7bc254b7e4a294338c48
Dao Le Bao Minh <djnner@proton.me>
2025-08-13 21:40:04 +0700

    sync main.cpp with template

--- main.cpp
+++ main.cpp
@@ -5,10 +5,8 @@
 #endif
 using namespace std;
 
-namespace {
 #define fi first
 #define se second
-#define all(x) (x).begin(), (x).end()
 using str = string;
 using ll = long long;
 using ld = long double;
@@ -16,16 +14,13 @@ using pll = pair<ll, ll>;
 using pld = pair<ld, ld>;
 using pii = pair<int, int>;
 
-#define gint                                                                   \
-  ([]() {                                                                      \
-    ll x;                                                                      \
-    return cin >> x, x;                                                        \
-  }())
-#define gstr                                                                   \
-  ([]() {                                                                      \
-    str x;                                                                     \
-    return cin >> x, x;                                                        \
-  }())
+template <class T> T read_() {
+  T x;
+  cin >> x;
+  return x;
+}
+#define gint read_<int>()
+#define gstr read_<str>()
 #define rpt(i, u, v) for (ll i = (u), _n_ = (v); i < _n_; i++)
 #define tpr(i, u, v) for (ll i = (u), _n_ = (v); --i >= _n_;)
 
@@ -37,18 +32,32 @@ template <class A, class B> bool chmax(A &a, const B b) {
 template <class A, class B> bool chmin(A &a, const B b) {
   return b < a && (a = b, 1);
 }
-} // namespace
-
 const int mod = 1000000007;
 const ld eps = 0.000000001, pi = acos(-1);
 const ll e30 = 1ll << 30, e60 = 1ll << 60;
 
-/* -----------------[ MAIN CODE GOES HERE ]----------------- */
+namespace config {
+const bool MULTI_TESTS = 1;
+const bool IS_INTERACTIVE = 0;
+const int FLOAT_PRECISION = 0;
+} // namespace config
+
+namespace me {
+auto stay_strong() {
+
+}
+} // namespace me
 
 int main() {
-  cin.tie(nullptr)->sync_with_stdio(false);
-
-  
+  if (!config::IS_INTERACTIVE)
+    cin.tie(nullptr)->sync_with_stdio(false);
+  if (config::FLOAT_PRECISION)
+    cout << fixed << setprecision(config::FLOAT_PRECISION);
+  int days_without_you = 1;
+  if (config::MULTI_TESTS)
+    cin >> days_without_you;
+  while (days_without_you--)
+    me::stay_strong();
 }
 
 /* -------
